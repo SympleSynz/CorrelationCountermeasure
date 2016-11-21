b0759acd296d17fcd490fa20b39da72eba1374d7
Erik Eakins <erik.eakins@colorado.edu>
2016-11-21 13:58:22 -0700

    Removed Stackoverflow code

--- spearman.py
+++ spearman.py
@@ -1,27 +1,5 @@
 import os
 import copy
-#Following code pulled from Stackoverflow to rank unsorted lists
-#http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
-def rank_simple(vector):
-    return sorted(range(len(vector)), key=vector.__getitem__)
-
-def rankdata(a):
-    n = len(a)
-    ivec=rank_simple(a)
-    svec=[a[rank] for rank in ivec]
-    sumranks = 0
-    dupcount = 0
-    newarray = [0]*n
-    for i in range(n):
-        sumranks += i
-        dupcount += 1
-        if i==n-1 or svec[i] != svec[i+1]:
-            averank = sumranks / float(dupcount) + 1
-            for j in range(i-dupcount+1,i+1):
-                newarray[ivec[j]] = averank
-            sumranks = 0
-            dupcount = 0
-    return newarray
 
 def Rank(data):
 	sortData = copy.deepcopy(data)
