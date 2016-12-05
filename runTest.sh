177dad2a5beff790b2d871ef408b7949cafeae4c
Mike Watson <watsonmf@gmail.com>
2016-12-05 06:37:57 -0700

    fix scripts

--- runTest.sh
+++ runTest.sh
@@ -7,7 +7,7 @@ click_file=$(pwd)/click_scripts/$1
 click_program=$(pwd)/click-master/installdir/bin/click
 
 results_dir=$(pwd)/results
-output_dir=$results_dir/(date +%Y%m%d%H%M%S)/
+output_dir=$results_dir/$(date +%Y%m%d%H%M%S)/
 
 mkdir $output_dir
 cd $output_dir
