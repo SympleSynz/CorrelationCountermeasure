b275fae7b2330e3dcba03fdce971c740fdffe6fd
Mike Watson <watsonmf@gmail.com>
2016-12-08 00:02:30 -0700

    test again

--- click-master/elements/local/coverSpike.hh
+++ click-master/elements/local/coverSpike.hh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef CLICK_COVERSPIKE_HH
 #define CLICK_COVERSPIKE_HH
 #include <click/element.hh>
@@ -41,47 +40,3 @@ class CoverSpike : public Element
 
 CLICK_ENDDECLS
 #endif
-=======
-#ifndef CLICK_COVERSPIKE_HH
-#define CLICK_COVERSPIKE_HH
-#include <click/element.hh>
-CLICK_DECLS
-
-#define NUM_FLOWS 51
-#define NUM_SPIKE 5
-
-typedef struct _flow2
-{
-	char address[16];
-	int active;
-} Flow2;
-
-class CoverSpike : public Element 
-{ 
-	public:
-
-  		int _prob;
-		Flow2* flowArray;
-
-		int bigSpike[NUM_SPIKE];
-		int midSpike[NUM_SPIKE];
-		int littleSpike[NUM_SPIKE];
-
-	public:
-  		CoverSpike();
-  		~CoverSpike();
-
-  		const char *class_name() const	{ return "CoverSpike"; }
-  		const char *port_count() const	{ return "1/1"; }
-  		const char *processing() const  { return PUSH; }
-
-  		void push(int, Packet *);
-  		int configure(Vector<String>&, ErrorHandler*);
-		void send_cover(int, Packet *);
-
-};
-
-
-CLICK_ENDDECLS
-#endif
->>>>>>> refs/remotes/origin/master
